796647a859932e4d1defec20317402c39e121747
Abhishek09yadav <monuy8830@gmail.com>
2025-03-13 22:15:02 +0530

    Add utility functions and button component; initialize AOS for animations and update global styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,10 @@
+"use client";
 import Image, { StaticImageData } from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 interface ProductProps {
   product: {
     id: number;
@@ -12,28 +17,46 @@ interface ProductProps {
 }
 
 const ProductCard: React.FC<ProductProps> = ({ product }) => {
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 1000,
+
+    });
+  })
+
   return (
-    <div className="flex flex-col items-center  rounded-lg shadow-md w-full sm:w-[45%] md:w-[30%] lg:[22%] transition-transform hover:scale-105 drop-shadow-md ">
+    <div
+      data-aos="fade-up"
+       className="flex flex-col items-center   rounded-lg shadow-md w-full sm:w-[45%] md:w-[30%] lg:[22%] transition-transform hover:scale-105 drop-shadow-md "
+    >
       <Image
         src={product.image}
         alt={product.name}
-        className="h-48 object-contain rounded-md w-full "
+        className="h-48 object-contain rounded-xl
+ w-full "
       />
       <h3 className="text-lg font-semibold mt-3 text-center text-avocado-600">
         {product.name}
       </h3>
       <h3 className="from-neutral-500 text-sm">Category: {product.category}</h3>
-      <div className="flex flex-col items-center w-full mt-3">
-        <div className=" text-xs text-neutral-400">
+      <div className="flex flex-row items-center justify-around  w-full m-3">
+        <div className="flex flex-col  gap-2 mt-3">
           {" "}
-          Original Price:{" "}
-          <span className="line-through text-xs">{product.old_price}</span>
+          <div className=" text-xs text-neutral-400">
+            Original Price:{" "}
+            <span className="line-through text-xs">{product.old_price}</span>
+          </div>
+          <div>
+            New Price:{" "}
+            <span className=" text-green-500 font-bold">
+              {product.new_price}
+            </span>
+          </div>
         </div>
-        <div>
-          {" "}
-          New Price:{" "}
-          <span className=" text-green-500 font-bold">{product.new_price}</span>
-        </div>
+
+        <Button>Shop Now</Button>
       </div>
     </div>
   );
