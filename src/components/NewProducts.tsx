dcaf4c882ea7360e1c014bad9e0297e5a75cae93
Abhishek09yadav <monuy8830@gmail.com>
2025-03-14 02:01:47 +0530

    Enhance NewProducts component with Particles background and update styles

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import all_product from '../../public/img/all_product.js' ;
 import { AuroraText } from "./magicui/aurora-text";
+import { Particles } from "./magicui/particles";
 
 
 
 
 const NewProducts = () => {
+    const [color, setColor] = useState("#019031");
   return (
     <div>
-      <div className="container px-4 pt-16">
+      <div className="container px-4 pt-16 relative">
         <h2 className="flex text-2xl pb-6 font-bold justify-center">
-          <AuroraText>  New Products</AuroraText>
+          <AuroraText className="font-bold text-5xl"> New Products</AuroraText>
         </h2>
-
+        <Particles
+          className="absolute inset-0 z-[-1] bg-red"
+          quantity={1000}
+          ease={80}
+          color={color}
+          refresh
+        />
         <div className="flex flex-wrap justify-center gap-6 mb-6">
           {all_product.map((value, index) => (
             <ProductCard key={index} product={value} />
