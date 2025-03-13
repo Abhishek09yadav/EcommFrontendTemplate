169bc334ad74d748936290ab0c6662d2a216a41f
Abhishek09yadav <monuy8830@gmail.com>
2025-03-13 20:49:11 +0530

    Refactor product images and update NewProducts component; remove unused images and enhance ProductCard layout

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -9,7 +9,7 @@ const NewProducts = () => {
   return (
     <div>
       <div className="container px-4 pt-16">
-        <h2 className="flex font-semibold  text-2xl pb-6  justify-center">
+        <h2 className="flex font-semibold  text-2xl pb-6 font-bold justify-center">
           New Products 
         </h2>
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
-import Image, { StaticImageData } from 'next/image';
-import React from 'react'
+import Image, { StaticImageData } from "next/image";
+import React from "react";
 interface ProductProps {
   product: {
     id: number;
@@ -13,20 +13,30 @@ interface ProductProps {
 
 const ProductCard: React.FC<ProductProps> = ({ product }) => {
   return (
-    <div className="flex flex-col items-center border-accent border-2 rounded-lg shadow-md w-full sm:w-[45%] md:w-[30%] lg:[22%] transition-transform hover:scale-105">
+    <div className="flex flex-col items-center  rounded-lg shadow-md w-full sm:w-[45%] md:w-[30%] lg:[22%] transition-transform hover:scale-105 drop-shadow-md ">
       <Image
         src={product.image}
         alt={product.name}
         className="h-48 object-contain rounded-md w-full "
       />
-      <h3 className="text-lg font-semibold mt-3 text-center text-avocado-600">{product.name}</h3>
+      <h3 className="text-lg font-semibold mt-3 text-center text-avocado-600">
+        {product.name}
+      </h3>
       <h3 className="from-neutral-500 text-sm">Category: {product.category}</h3>
-      <div className="flex justify-between w-full mt-2">
-        <span>{product.new_price}</span>
-        <span>{product.old_price}</span>
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className=" text-xs text-neutral-400">
+          {" "}
+          Original Price:{" "}
+          <span className="line-through text-xs">{product.old_price}</span>
+        </div>
+        <div>
+          {" "}
+          New Price:{" "}
+          <span className=" text-green-500 font-bold">{product.new_price}</span>
+        </div>
       </div>
     </div>
   );
 };
 
-export default ProductCard
+export default ProductCard;
